6fc083d00e84de37b97f805ce7ce841365895e10
karlosusac <karlosusac97@gmail.com>
2020-02-18 10:38:28 +0100

    Added 'View Binding' and Progress Bar loading

--- app/src/main/java/com/redomat/improved/LoginActivity.java
+++ app/src/main/java/com/redomat/improved/LoginActivity.java
@@ -2,7 +2,9 @@ package com.redomat.improved;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -14,11 +16,14 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.redomat.improved.databinding.ActivityLoginBinding;
 import com.redomat.improved.pojo.Account;
 
 public class LoginActivity extends AppCompatActivity {
@@ -27,9 +32,22 @@ public class LoginActivity extends AppCompatActivity {
     private FirebaseUser currentUser;
     private FirebaseDatabase db = FirebaseDatabase.getInstance();
 
-    //Initializing activity variables
+    //View Binding
+    private ActivityLoginBinding mBiding;
+    //---------------------------------
+    
 
+    //Initializing activity variables
+    private TextInputLayout logInputEmail;
+    private TextInputLayout logInputPass;
+
+    private Button logBtnLogin;
+    private Button logBtnEnterALine;
     //--------------------------------
+
+    //Progress dialog
+    ProgressDialog progressDialog;
+    //-------------
 
 
     @Override
@@ -43,16 +61,42 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        mBiding = ActivityLoginBinding.inflate(getLayoutInflater());
+        View loginView = mBiding.getRoot();
+        setContentView(loginView);
 
-        EditText logInputEmail = findViewById(R.id.loginInputEmail);
-        EditText logInputPass = findViewById(R.id.loginInputPassword);
+        //mBinding = DataBindingUtil.setContentView(this, R.layout.activity_login);
 
-        Button logBtnLogin = findViewById(R.id.loginBtnLogin);
-        Button logBtnEnterALine = findViewById(R.id.loginBtnEnterALine);
+        //Layout elements
+        //EditText
+        logInputEmail = mBiding.loginInputEmail;
+        logInputPass = mBiding.loginInputPassword;
 
+        //Buttons
+        logBtnLogin = mBiding.loginBtnLogin;
+        logBtnEnterALine = mBiding.loginBtnEnterALine;
+
+        /*
+        final TextInputLayout logInputEmail = mBinding.loginInputEmail;
+
+        final Button logBtnLogin = mBinding.loginBtnLogin;
+
+        logBtnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(LoginActivity.this, logInputEmail.getEditText().getText().toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+         */
     }
 
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        closeProgressDialog();
+
+    }
 
     //Functions
     public void  updateUI(FirebaseUser user){
@@ -60,5 +104,31 @@ public class LoginActivity extends AppCompatActivity {
             startActivity(new Intent(this, MainMenuActivity.class));
             finish();
         }
+    }
+
+    //Function to go to Register Acitivity - Used in XML as a link
+    public void openRegisterActivity(View v){
+        showProgressDialog();
+
+        Intent newIntent = new Intent(this, RegisterActivity.class);
+        startActivity(newIntent);
+    }
+
+    //Make new Progress dialog for loading screen
+    public void showProgressDialog(){
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setCancelable(false);
+        progressDialog.setCanceledOnTouchOutside(false);
+
+        progressDialog.show();
+        progressDialog.setContentView(R.layout.dialog_progress);
+        progressDialog.getWindow().setBackgroundDrawableResource(
+                android.R.color.transparent
+        );
+    }
+
+    //Close progress dialog
+    public void closeProgressDialog(){
+        progressDialog.dismiss();
     }
 }
--- app/src/main/java/com/redomat/improved/RegisterActivity.java
+++ app/src/main/java/com/redomat/improved/RegisterActivity.java
@@ -2,13 +2,57 @@ package com.redomat.improved;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+import com.redomat.improved.databinding.ActivityRegisterBinding;
 
 public class RegisterActivity extends AppCompatActivity {
+    //Firebase stuff
+    private FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    private FirebaseDatabase db = FirebaseDatabase.getInstance();
+
+    //View Binding
+    private ActivityRegisterBinding mBiding;
+    //--------------------------------------
+
+    //Initializing activity variables
+    private TextInputLayout regInputEmail;
+    //--------------------------------
+
+    //Progress dialog
+    ProgressDialog progressDialog;
+    //-------------
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        mBiding = ActivityRegisterBinding.inflate(getLayoutInflater());
+        View registerView = mBiding.getRoot();
+        setContentView(registerView);
+    }
+
+    //Function to go back to Login Acitivity - Used in XML as a link
+    public void openLoginActivity(View v){
+        showProgressDialog();
+        finish();
+    }
+
+    //Make new Progress dialog for loading screen
+    public void showProgressDialog(){
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setCancelable(false);
+        progressDialog.setCanceledOnTouchOutside(false);
+
+        progressDialog.show();
+        progressDialog.setContentView(R.layout.dialog_progress);
+        progressDialog.getWindow().setBackgroundDrawableResource(
+                android.R.color.transparent
+        );
     }
 }
